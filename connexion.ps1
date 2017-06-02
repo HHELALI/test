9a796c4b5f53a52c17f847751a68fe0fa8741fab
hiba <hhelali@solocal.com>
2017-06-02 16:09:54 +0200

    message de commit

---
+++ connexion.ps1
@@ -0,0 +1,10 @@
+﻿Set-Executionpolicy -Scope CurrentUser -ExecutionPolicy UnRestricted
+$User = "admin"
+$PWord = ConvertTo-SecureString –String "azerty" –AsPlainText -Force
+$Credential = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $User, $PWord
+
+
+Enable-PSRemoting –force
+
+
+Invoke-Command -ComputerName 10.20.75.229 -Credential $Credential -ScriptBlock {New-Item -Path . -Name "testfile1.txt" -ItemType "file" -Value "This is a text string."}
